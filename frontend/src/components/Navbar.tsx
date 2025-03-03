b55685b88e8c9cbb8e5065184bb5ff31061b639a
Nauman Khan <thetopofsoup@gmail.com>
2025-03-02 21:04:27 -0500

    Fix code style issues with Prettier

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import "../styles/Navbar.css"
+import React from 'react';
+import '../styles/Navbar.css';
 
+const Navbar: React.FC = () => {
+  return (
+    <nav className="Navbar">
+      <h1>FILM BUDGET TRACKER</h1>
+      <ul>
+        <li>
+          <a href="/">Dashboard</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-const Navbar : React.FC= () => {
-    return (
-        <nav className="Navbar">
-            <h1>FILM BUDGET TRACKER</h1>
-            <ul>
-                <li><a href="/">Dashboard</a></li>
-            </ul>
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
