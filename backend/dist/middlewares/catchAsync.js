3453b84383738202a42e567b8e53f5483dbb77d4
Nauman Khan <thetopofsoup@gmail.com>
2025-05-05 17:26:24 -0400

    chore: ignore and untrack backend dist directory

--- backend/dist/middlewares/catchAsync.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-// Ensure the return type is correct
-const catchAsync = (fn) => (req, res, next) => {
-  fn(req, res, next).catch(next);
-};
-exports.default = catchAsync;
